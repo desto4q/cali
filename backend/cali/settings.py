9e8957e7df142e0d47fca17b84dc7084eff58a78
desto4q <desto4q@gmail.com>
2023-06-13 02:59:54 +0100

    fixes

--- backend/cali/settings.py
+++ backend/cali/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ch))s13v*)*8&!2hw-hj66k(b)5vq#zgesnb_o70jg)(+^(t='
+SECRET_KEY = 'django-insecure-qcd$_-+lmr3^p_otfo+_ud!pg@$hl=n3f^7##m1f@5ov^2rwr^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
