f366c9ba2b99795d821215822cd2e72735446574
desto4q <desto4q@gmail.com>
2023-06-28 15:31:47 +0100

    fixing id

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -10,10 +10,10 @@ function UserProfile() {
   return (
     <div className="userProfile">
         {user.profileImg ? <img src={user.profileImg} alt="" /> : <>
-        <div className='imgHolder'>{user.username? user.username[0].toUpperCase(): "G"}</div>
+        <div className='imgHolder'>{user.username ? user.username[0].toUpperCase(): "G"}</div>
         </>}
         <div className="details">
-            <h3>{user.username? user.username  : "Guest"}</h3>
+            <h3>{user.username ? user.username  : "Guest"}</h3>
             <p>{user.id ? `#${user.id}` : "#000000"}</p>
         </div>
     </div>
